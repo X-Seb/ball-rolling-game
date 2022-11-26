b0c892d5b2e8ade7c86f7b2917c3f3723faaeccf
Random Game Dev <102157648+Random-Game-Dev@users.noreply.github.com>
2022-11-26 17:30:39 -0500

    Changed the settings manager script

--- ball-rolling-game/Assets/_BallRollingGame_GameAssets/Managers/SettingsManager.cs
+++ ball-rolling-game/Assets/_BallRollingGame_GameAssets/Managers/SettingsManager.cs
@@ -2,16 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsManager : MonoBehaviour
 {
     public AudioMixer mainMixer;
+    public Slider _slider;
 
     private void Start()
     {
         if (PlayerPrefs.HasKey("Volume"))
         {
             mainMixer.SetFloat("Volume", PlayerPrefs.GetFloat("Volume"));
+            _slider.value = PlayerPrefs.GetFloat("Volume");
         }
     }
 
