1e034fb0f51df4dc7f05f6a54c395d973a57b44c
Random Game Dev <sebthproductivity@gmail.com>
2023-01-27 19:01:31 -0500

    WebGL works now
    I had to remove the LoadSceneAsync function

--- Just a ball-rolling game/Assets/_BallRollingGame_GameAssets/Singletons/LevelLoader.cs
+++ Just a ball-rolling game/Assets/_BallRollingGame_GameAssets/Singletons/LevelLoader.cs
@@ -13,8 +13,6 @@ public class LevelLoader : MonoBehaviour
     public static LevelLoader instance;
     [Header("UI stuff: ")]
     [SerializeField] private GameObject loadingCanvas;
-    [SerializeField] private Slider slider;
-    [SerializeField] private TextMeshProUGUI progressText;
     [SerializeField] private TextMeshProUGUI funnyText;
 
     [Header("Other Scripts elements specific to certain scenes: ")]
@@ -25,10 +23,6 @@ public class LevelLoader : MonoBehaviour
 
     [Header("This is the list of available string for the funnyText: ")]
     [SerializeField] private List<string> funnyTextOptions;
-
-    [Header("Progress bar variables: ")]
-    [SerializeField] float targetSliderValue = 0.0f;
-    [SerializeField] bool _isSliderVisible = false;
 
     private void Awake()
     {
@@ -47,20 +41,7 @@ public class LevelLoader : MonoBehaviour
 
     private void Start()
     {
-        slider.value = 0.0f;
-        targetSliderValue = 0.0f;
-
         GetMainMenuManager();
-    }
-
-    private void Update()
-    {
-        //If it's visible, change the slider's value and the text percentage
-        if (_isSliderVisible == true)
-        {
-            slider.value = Mathf.MoveTowards(slider.value, targetSliderValue, 100 * Time.deltaTime);
-            progressText.text = Mathf.Round((Mathf.MoveTowards(slider.value, targetSliderValue, 100 * Time.deltaTime))).ToString() + "%";
-        }
     }
 
     //This functions loads the next scene
@@ -84,46 +65,80 @@ public class LevelLoader : MonoBehaviour
             UIManager.instance.ChangingScene();
         }
 
-        //Randomizes the funny text + resets the slider's target value to 0.
+        // Randomizes the funny text + resets the slider's target value to 0.
         RandomizeFunnyText();
-        targetSliderValue = 0;
 
         var scene = SceneManager.LoadSceneAsync(sceneBuildIndex);
 
-        //This makes it so the scene doesn't change on its own.
+        // This makes it so the scene doesn't change on its own.
         scene.allowSceneActivation = false;
 
         loadingCanvas.SetActive(true);
-        _isSliderVisible = true;
         GameManager.instance.SetGameState(GameManager.GameState.LEAVING_SCENE);
 
         do
         {
-            //Update the visual indicators to show the loading progress
-            targetSliderValue = scene.progress * 111;
+            // Update the visual indicators to show the loading progress
+            //targetSliderValue = scene.progress * 111;
         } while (scene.progress < 0.9f);
 
-        //Wait one and a half seconds before changing scenes and reseting everything
+        // Wait one and a half seconds before changing scenes and reseting everything
         yield return new WaitForSeconds(1.5f);
         AudioSingleton.Instance.StopMusic();
 
         Debug.Log("Scene loaded!");
-        //Allow the next scene to activate, which changes the scene
+        // Allow the next scene to activate, which changes the scene
         scene.allowSceneActivation = true;
 
-        //Turn off the loading screen and reset certain values
+        // Turn off the loading screen and reset certain values
         loadingCanvas.SetActive(false);
-        _isSliderVisible = false;
-        targetSliderValue = 0;
-        slider.value = 0;
+        //_isSliderVisible = false;
+        //targetSliderValue = 0;
+        //slider.value = 0;
 
         yield return new WaitForSeconds(1.5f);
         GetMainMenuManager();
     }
 
+    // This doesn't really load the scene asyncronously anymore due to WebGL problems
     public void LoadSceneAsync(int sceneBuildIndex)
     {
-        StartCoroutine(LoadScene(sceneBuildIndex));
+        Debug.Log("Loading the next scene.");
+
+        AudioSingleton.Instance.SetVolumeGradually(0.0f, 1.5f);
+        //Unpauses the game so the UI displays correctly.
+        Time.timeScale = 1.0f;
+
+        //Checks if you're in a game Level to use the UIManager, which is only present in levels
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            GetMainMenuManager();
+            mainMenuManager.LeavingScene();
+        }
+        if (SceneManager.GetActiveScene().buildIndex != 0)
+        {
+            //Deactivate the mainCanvas
+            UIManager.instance.ChangingScene();
+        }
+
+        RandomizeFunnyText();
+        loadingCanvas.SetActive(true);
+        GameManager.instance.SetGameState(GameManager.GameState.LEAVING_SCENE);
+        AudioSingleton.Instance.StopMusic();
+
+
+        // Actually load the scene
+        SceneManager.LoadScene(sceneBuildIndex);
+
+        loadingCanvas.SetActive(false);
+        GetMainMenuManager();
+
+        //StartCoroutine(LoadScene(sceneBuildIndex));
+    }
+
+    private IEnumerator LoadSceneInstantly()
+    {
+        yield return null;
     }
 
     private void RandomizeFunnyText()
